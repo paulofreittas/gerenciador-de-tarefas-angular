600c12f7ed3b8f0dc7d60861c59266484bdb2448
paulofreittas <paulo-henriquee@hotmail.com>
2017-08-30 02:41:09 -0300

    Criação do Serviço e dos componentes de listar e cadastrar tarefa.

---
+++ src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { TarefaService } from './shared';
+import { ListarTarefaComponent } from './listar';
+import { CadastrarTarefaComponent } from './cadastrar';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule
+  ],
+  declarations: [
+    ListarTarefaComponent,
+    CadastrarTarefaComponent
+  ],
+  providers: [
+    TarefaService
+  ]
+})
+export class TarefasModule { }
